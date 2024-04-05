e86cedb9ae0c5b26f12775cf861e0b93e1214fe9
OmerFarukArpa <arpaomer03@gmail.com>
2024-04-05 12:17:41 +0300

    Update App.js

--- ilk/firstapp/App.js
+++ ilk/firstapp/App.js
@@ -17,17 +17,45 @@ export default function App() {
         source={require('./assets/resim.jpg')} // İlk restoran için uygun bir resim dosyasının yolunu girin
         
       />
+       <Text style={styles.title}>Favori Restoranlar</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="1. restorant"
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
+      />
       <Image
         style={styles.restaurantImage}
         source={require('./assets/resim2.jpg')} // İkinci restoran için uygun bir resim dosyasının yolunu girin
+      />
+       <Text style={styles.title}>Favori Restoranlar</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="2. restorant"
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
       />
       <Image
         style={styles.restaurantImage}
         source={require('./assets/resim3.jpg')} //  
       />
+       <Text style={styles.title}>Favori Restoranlar</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="3. restorant"
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
+      />
       <Image
         style={styles.restaurantImage}
         source={require('./assets/resim4.jpg')} //  
+      />
+       <Text style={styles.title}>Favori Restoranlar</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="4. restorant"
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
       />
        <Image
         style={styles.restaurantImage}
@@ -76,5 +104,12 @@ const styles = StyleSheet.create({
   searchText: {
     fontSize: 16,
     color: '#888', // Arama çubuğu metni rengi
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
   },
 });
